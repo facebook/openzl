2ab66c866e1223f87b55eb83b15d35734d7db10f
Yann Collet <Cyan4973@users.noreply.github.com>
2025-10-04 07:07:14 +0000

    fix clang-format issue

--- include/openzl/zl_config.h
+++ include/openzl/zl_config.h
@@ -7,26 +7,28 @@
 // These macros control optional features and optimizations.
 // Values can be overridden using preprocessor variables (-D flags in CPPFLAGS)
 
-// Ensure all configuration defines have defaults if not provided by build system
+// Ensure all configuration defines have defaults if not provided by build
+// system
 #ifndef ZL_ALLOW_INTROSPECTION
-# define ZL_ALLOW_INTROSPECTION 1
+#    define ZL_ALLOW_INTROSPECTION 1
 #endif
 
 #ifndef ZL_HAVE_FBCODE
-# define ZL_HAVE_FBCODE 0
+#    define ZL_HAVE_FBCODE 0
 #endif
 
 #ifndef ZL_HAVE_X86_64_ASM
-# define ZL_HAVE_X86_64_ASM 0
+#    define ZL_HAVE_X86_64_ASM 0
 #endif
 
 // BEGIN CODEMOD DEFINES - Legacy compatibility
 #ifndef OPENZL_HAVE_FBCODE
-# define OPENZL_HAVE_FBCODE ZL_HAVE_FBCODE // TODO(T223464378): Delete
+#    define OPENZL_HAVE_FBCODE ZL_HAVE_FBCODE // TODO(T223464378): Delete
 #endif
 #ifndef OPENZL_HAVE_X86_64_ASM
-# define OPENZL_HAVE_X86_64_ASM ZL_HAVE_X86_64_ASM // TODO(T223464378): Delete
+#    define OPENZL_HAVE_X86_64_ASM \
+        ZL_HAVE_X86_64_ASM // TODO(T223464378): Delete
 #endif
 // END CODEMOD DEFINES
 
-#endif  // OPENZL_ZL_CONFIG_H
+#endif // OPENZL_ZL_CONFIG_H
