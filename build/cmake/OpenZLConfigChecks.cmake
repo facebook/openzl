7ae2c9c70df3c8648474936be9bd6b7bb04222ef
Yann Collet <Cyan4973@users.noreply.github.com>
2025-10-04 05:48:43 +0000

    fixed cmake build issues

--- build/cmake/OpenZLConfigChecks.cmake
+++ build/cmake/OpenZLConfigChecks.cmake
@@ -3,13 +3,13 @@
 # This file handles configuration of what features we have based on dynamic build
 # time checks.
 
-set(OPENZL_HAVE_X86_64_ASM FALSE)
+set(CMAKE_DETECTED_X86_64_ASM_SUPPORT FALSE)
 if (${IS_X86_64_ARCH})
     if (NOT MSVC)
         message(
             STATUS
             "Enabling x86-64 assembly"
         )
-        set(OPENZL_HAVE_X86_64_ASM TRUE)
+        set(CMAKE_DETECTED_X86_64_ASM_SUPPORT TRUE)
     endif()
 endif()
