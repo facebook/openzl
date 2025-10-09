91654a98f0fec265695d5d960cc0dac44da6384d
Nathan Delisle <ndelisle@uchicago.edu>
2025-10-08 21:33:47 -0500

    style: clang-format

--- tools/io/InputSetBuilder.h
+++ tools/io/InputSetBuilder.h
@@ -3,11 +3,10 @@
 #pragma once
 
 #include <memory>
-#include <vector>
-#include <string>
 #include <optional>
 #include <stdexcept>
-
+#include <string>
+#include <vector>
 
 #include "tools/io/InputSet.h"
 
@@ -17,23 +16,23 @@ namespace openzl::tools::io {
  * Helper class to build up an input set from a bunch of path arguments.
  */
 class InputSetBuilder {
-   public:
-    explicit InputSetBuilder(bool recursive, bool verbose = false);
+public:
+  explicit InputSetBuilder(bool recursive, bool verbose = false);
 
-    InputSetBuilder& add_path(std::string path) &;
-    InputSetBuilder&& add_path(std::string path) &&;
+  InputSetBuilder &add_path(std::string path) &;
+  InputSetBuilder &&add_path(std::string path) &&;
 
-    InputSetBuilder& add_path(std::optional<std::string> path_opt) &;
-    InputSetBuilder&& add_path(std::optional<std::string> path_opt) &&;
+  InputSetBuilder &add_path(std::optional<std::string> path_opt) &;
+  InputSetBuilder &&add_path(std::optional<std::string> path_opt) &&;
 
-    std::unique_ptr<InputSet> build() &&;
+  std::unique_ptr<InputSet> build() &&;
 
-    std::unique_ptr<InputSet> build_static() &&;
+  std::unique_ptr<InputSet> build_static() &&;
 
-   private:
-    const bool recursive_;
-    const bool verbose_;
+private:
+  const bool recursive_;
+  const bool verbose_;
 
-    std::vector<std::unique_ptr<InputSet>> input_sets_;
+  std::vector<std::unique_ptr<InputSet>> input_sets_;
 };
 } // namespace openzl::tools::io
