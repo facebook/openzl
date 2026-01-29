3fe988c535ee357525548ce3a29b1c53cd78ba8c
Yann Collet <cyan@meta.com>
2026-01-29 15:33:41 -0800

    use `clang-format`

--- src/openzl/codecs/bitSplit/decode_bitSplit_kernel.c
+++ src/openzl/codecs/bitSplit/decode_bitSplit_kernel.c
@@ -6,7 +6,7 @@
 
 static inline void ZS_decodeElements(
         void* dst,
-        size_t dstEltWidth,
+        const size_t dstEltWidth,
         size_t nbElts,
         const void* const srcPtrs[],
         const size_t* srcEltWidths,
@@ -15,21 +15,31 @@ static inline void ZS_decodeElements(
 {
     for (size_t e = 0; e < nbElts; e++) {
         uint64_t value = 0;
-        size_t bitPos = 0;
+        size_t bitPos  = 0;
 
         for (size_t i = 0; i < nbWidths; i++) {
             uint64_t part = 0;
             switch (srcEltWidths[i]) {
-                case 1: part = ((const uint8_t*)srcPtrs[i])[e]; break;
-                case 2: part = ((const uint16_t*)srcPtrs[i])[e]; break;
-                case 4: part = ((const uint32_t*)srcPtrs[i])[e]; break;
-                case 8: part = ((const uint64_t*)srcPtrs[i])[e]; break;
+                case 1:
+                    part = ((const uint8_t*)srcPtrs[i])[e];
+                    break;
+                case 2:
+                    part = ((const uint16_t*)srcPtrs[i])[e];
+                    break;
+                case 4:
+                    part = ((const uint32_t*)srcPtrs[i])[e];
+                    break;
+                case 8:
+                    part = ((const uint64_t*)srcPtrs[i])[e];
+                    break;
             }
 
             value |= (part << bitPos);
             bitPos += bitWidths[i];
         }
 
+        // If `dstEltWidth` is a constant and `ZS_decodeElements()` is inlined,
+        // we expect the compiler to fold this switch() statement.
         switch (dstEltWidth) {
             case 1:
                 ((uint8_t*)dst)[e] = (uint8_t)value;
@@ -44,9 +54,7 @@ static inline void ZS_decodeElements(
                 ((uint64_t*)dst)[e] = (uint64_t)value;
                 break;
         }
-
     }
-
 }
 
 void ZS_bitSplitDecode(
@@ -78,19 +86,24 @@ void ZS_bitSplitDecode(
         assert(sumBitWidths <= dstEltWidth * 8);
     }
 
-    // We expect the compiler to optimize ZS_decodeElements() by propagating the constant (thus resulting in several instances).
+    // We expect the compiler to optimize ZS_decodeElements() by propagating the
+    // constant (thus resulting in several instances).
     switch (dstEltWidth) {
         case 1:
-            ZS_decodeElements(dst, 1, nbElts, srcPtrs, srcEltWidths, bitWidths, nbWidths);
+            ZS_decodeElements(
+                    dst, 1, nbElts, srcPtrs, srcEltWidths, bitWidths, nbWidths);
             break;
         case 2:
-            ZS_decodeElements(dst, 2, nbElts, srcPtrs, srcEltWidths, bitWidths, nbWidths);
+            ZS_decodeElements(
+                    dst, 2, nbElts, srcPtrs, srcEltWidths, bitWidths, nbWidths);
             break;
         case 4:
-            ZS_decodeElements(dst, 4, nbElts, srcPtrs, srcEltWidths, bitWidths, nbWidths);
+            ZS_decodeElements(
+                    dst, 4, nbElts, srcPtrs, srcEltWidths, bitWidths, nbWidths);
             break;
         case 8:
-            ZS_decodeElements(dst, 8, nbElts, srcPtrs, srcEltWidths, bitWidths, nbWidths);
+            ZS_decodeElements(
+                    dst, 8, nbElts, srcPtrs, srcEltWidths, bitWidths, nbWidths);
             break;
     }
 }
