e511dd9a36eb08c20d709524e0d6196b901d34ef
drozenblit <drozenblit@fb.com>
2026-02-09 12:40:22 -0800

    Easy: More-intuitive input/output specification in the compiler cli
    Summary: Modifies the cli to specify input/output files as flagged arguments. Requires an input to be specified, but generates a canonical output if unspecified.
    
    Differential Revision: D92720731

--- tools/sddl2/compiler/main.cpp
+++ tools/sddl2/compiler/main.cpp
@@ -1,12 +1,14 @@
 // Copyright (c) Meta Platforms, Inc. and affiliates.
-
-#include <iostream>
-#include <iterator>
+#include <filesystem>
 #include <string>
 
 #include "tools/sddl2/compiler/Compiler.h"
 #include "tools/sddl2/compiler/Exception.h"
 
+#include "tools/io/InputFile.h"
+#include "tools/io/OutputFile.h"
+
+using namespace openzl;
 using namespace openzl::sddl2;
 
 namespace {
@@ -17,13 +19,17 @@ const char* const help_message =
         "Options:\n"
         "  -h  Print this help message.\n"
         "  -v  Increase verbosity.\n"
-        "  -q  Decrease verbosity.\n";
+        "  -q  Decrease verbosity.\n"
+        "  -i  Input file.\n"
+        "  -o  Output file.\n";
 
 }
 
 int main(int argc, char* argv[])
 {
     int verbosity = 0;
+    std::shared_ptr<tools::io::InputFile> input_file;
+    std::shared_ptr<tools::io::OutputFile> output_file;
     for (int i = 1; i < argc; i++) {
         if (argv[i] == std::string{ "-v" }) {
             verbosity++;
@@ -32,6 +38,20 @@ int main(int argc, char* argv[])
         } else if (argv[i] == std::string{ "-h" }) {
             std::cerr << help_message << std::endl;
             return 0;
+        } else if (argv[i] == std::string{ "-i" }) {
+            if (i + 1 >= argc) {
+                std::cerr << "Missing input file." << std::endl;
+                std::cerr << help_message << std::endl;
+                return 1;
+            }
+            input_file = std::make_shared<tools::io::InputFile>(argv[++i]);
+        } else if (argv[i] == std::string{ "-o" }) {
+            if (i + 1 >= argc) {
+                std::cerr << "Missing output file." << std::endl;
+                std::cerr << help_message << std::endl;
+                return 1;
+            }
+            output_file = std::make_shared<tools::io::OutputFile>(argv[++i]);
         } else {
             std::cerr << "Unrecognized option." << std::endl;
             std::cerr << help_message << std::endl;
@@ -40,12 +60,20 @@ int main(int argc, char* argv[])
     }
 
     try {
-        std::istreambuf_iterator<char> begin{ std::cin }, end;
-        const auto input = std::string{ begin, end };
+        if (!input_file) {
+            throw std::runtime_error("No input file specified.");
+        }
+        if (!output_file) {
+            std::filesystem::path path{ std::string(input_file->name()) };
+            output_file = std::make_shared<tools::io::OutputFile>(
+                    path.replace_extension("asm"));
+        }
         const auto compiler =
                 Compiler{ Compiler::Options{}.with_verbosity(verbosity) };
-        const auto compiled = compiler.compile(input, "[stdin]");
-        std::cout << compiled;
+        const auto compiled =
+                compiler.compile(input_file->contents(), input_file->name());
+
+        output_file->write(compiled);
     } catch (const CompilerException& ex) {
         if (verbosity >= -1) {
             std::cerr << "Compilation failed:\n";
