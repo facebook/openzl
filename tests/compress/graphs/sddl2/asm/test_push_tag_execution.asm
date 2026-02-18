c7daca3bcc985618713e145a40b1585275ceedbf
Danielle Rozenblit <drozenblit@meta.com>
2026-02-17 18:01:33 -0800

    Migrate VM and assembly execution tests to GTEST (#401)
    Summary:
    Pull Request resolved: https://github.com/facebook/openzl/pull/401
    
    # Stack
    
    The objective of this stack is to introduce a C++ implementation of the SDDL2 assembler.
    
    # Diff
    
    This diff migrates C tests to C++, leveraging GTEST and the C++ assembler API instead of the previous bytecode generation scripts. All assembly tests have been consolidated into `test_sddl2_assembly_execution.cpp`.
    
    Reviewed By: Cyan4973
    
    Differential Revision: D93271635
    
    fbshipit-source-id: 75a82cd4d25f9d3495ebfeeea66672fdd43d675a

--- tests/compress/graphs/sddl2/asm/test_push_tag_execution.asm
+++
@@ -1,7 +0,0 @@
-# Test: Push tag value onto the stack
-# Tests that push.tag opcode executes successfully without errors
-# Pushes tag value 100 onto the stack, then halts
-# Expected: No segments created, program completes successfully
-
-push.tag 100
-halt
--- tests/compress/graphs/sddl2/asm/test_segment_unspecified.asm
+++
@@ -1,7 +0,0 @@
-# Test: Simple segment creation (unspecified type)
-# Input: 5
-# Tests: Create one segment with size 5
-
-push.i32 5
-segment.create_unspecified
-halt
