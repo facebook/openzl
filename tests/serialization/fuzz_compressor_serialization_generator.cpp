93783ce79f7b683c822e747c7f7633c54d33f306
Kevin Zhang <kevz8@meta.com>
2026-03-05 16:01:29 -0800

    Use new registry in deserialization fuzzing (#469)
    Summary:
    Pull Request resolved: https://github.com/facebook/openzl/pull/469
    
    Add deserialization into compression round trip for simple graphs in fuzzing.
    
    Reviewed By: terrelln
    
    Differential Revision: D93520639
    
    fbshipit-source-id: 4d47bf5915b02d85a7a18dc16780b804eef03e0c

--- tests/serialization/fuzz_compressor_serialization_generator.cpp
+++ tests/serialization/fuzz_compressor_serialization_generator.cpp
@@ -14,6 +14,8 @@
 
 #include "tests/datagen/random_producer/PRNGWrapper.h"
 #include "tests/datagen/structures/CompressorProducer.h"
+#include "tests/registry/OpenZLComponents.h"
+#include "tests/utils.h"
 
 namespace {
 
@@ -37,10 +39,56 @@ std::vector<std::string> generateFuzzDeserializationCorpus()
     return corpus;
 }
 
+std::vector<std::string> generateFuzzDeserializeAndCompressSimpleCorpus()
+{
+    std::random_device rd;
+    auto gen = std::make_shared<std::mt19937>(rd());
+    auto rw  = std::make_shared<datagen::PRNGWrapper>(gen);
+    datagen::DataGen datagen(rw);
+    std::vector<std::string> corpus;
+    for (uint16_t componentId = 0;
+         componentId < (uint16_t)OpenZLComponentID::NumComponents;
+         componentId++) {
+        for (uint32_t i = 0; i < 100; ++i) {
+            auto randomSeed = rw->u32("seed");
+            auto openzlComponent =
+                    makeOpenZLComponent((OpenZLComponentID)componentId);
+            openzl::Compressor compressor;
+            std::vector<openzl::GraphID> graphs =
+                    openzlComponent->predefinedGraphs(compressor);
+            for (auto graph :
+                 openzlComponent->generateGraphs(compressor, datagen, 3)) {
+                graphs.push_back(graph);
+            }
+
+            for (auto node : openzlComponent->predefinedNodes(compressor)) {
+                graphs.push_back(buildTrivialGraph(compressor.get(), node));
+            }
+            for (auto node :
+                 openzlComponent->generateNodes(compressor, datagen, 3)) {
+                graphs.push_back(buildTrivialGraph(compressor.get(), node));
+            }
+            for (auto graph : graphs) {
+                compressor.selectStartingGraph(graph);
+                auto serializedCompressor = compressor.serialize();
+                std::string info;
+                info.reserve(6 + serializedCompressor.size());
+                info.append(reinterpret_cast<const char*>(&randomSeed), 4);
+                info.append(reinterpret_cast<const char*>(&componentId), 2);
+                info.append(serializedCompressor);
+                corpus.emplace_back(std::move(info));
+            }
+        }
+    }
+    return corpus;
+}
+
 std::optional<std::vector<std::string>> generateCorpus(std::string_view harness)
 {
     if (harness == "FuzzDeserialization") {
         return generateFuzzDeserializationCorpus();
+    } else if (harness == "FuzzDeserializeAndCompressSimple") {
+        return generateFuzzDeserializeAndCompressSimpleCorpus();
     } else if (harness == "FuzzRandomCompressorDeserializesSuccessfully") {
         return std::vector<std::string>{};
     } else {
