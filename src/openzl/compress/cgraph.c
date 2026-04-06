e6968aa02ffac75110e2b4e36a819c7bc43df584
Victor Zhang <csv@meta.com>
2026-04-06 14:05:27 -0700

    merge cgraph_validation into cgraph (#596)
    Summary:
    Pull Request resolved: https://github.com/facebook/openzl/pull/596
    
    There's no longer a good reason to split out cgraph_validation as its own file, because the logic has shrunk significantly and can fit into cgraph.c without causing problems.
    
    Reviewed By: terrelln
    
    Differential Revision: D98320755
    
    fbshipit-source-id: 90355c4115b3b66e6f0fa66956be3f689a9cfdd1

--- src/openzl/compress/cgraph.c
+++ src/openzl/compress/cgraph.c
@@ -91,6 +91,31 @@ const GCParams* CGRAPH_getGCParams(const ZL_Compressor* cgraph)
     return &cgraph->gcparams;
 }
 
+static ZL_Report CGraph_validateGraphAtGid(
+        ZL_Compressor* cgraph,
+        ZL_GraphID gid)
+{
+    ZL_RESULT_DECLARE_SCOPE_REPORT(cgraph);
+    ZL_DLOG(BLOCK, "CGraph_validateGraphAtGid (%u)", gid.gid);
+
+    ZL_ERR_IF_NOT(ZL_GraphID_isValid(gid), graph_invalid);
+
+    return ZL_returnSuccess();
+}
+
+ZL_Report ZL_Compressor_validate(
+        ZL_Compressor* cgraph,
+        const ZL_GraphID starting_graph)
+{
+    ZL_RESULT_DECLARE_SCOPE_REPORT(cgraph);
+    ZL_DLOG(BLOCK, "ZL_Compressor_validate");
+    // Check that we start with a valid gid
+    ZL_ERR_IF_ERR(CGraph_validateGraphAtGid(cgraph, starting_graph));
+    // Note (@Cyan): since zstrong supports Typed Inputs, there is no longer a
+    // requirement for Starting Graph to support Serial Input.
+    return ZL_returnSuccess();
+}
+
 // ******************************************************************
 // CGraph creation
 // ******************************************************************
