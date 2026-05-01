78f3dce94bfc2b44143fdd6083d7ea969395afe2
Nick Terrell <terrelln@meta.com>
2026-05-01 14:21:11 -0700

    Stop using the serialized compressor in clustering training (#713)
    Summary:
    
    Update clustering training to stop querying & mutating the serialized graph. Instead do all queries and modifications through the `Compressor` directly.
    
    Reviewed By: kevinjzhang
    
    Differential Revision: D103275004

--- tools/training/clustering/clustering_graph_trainer.cpp
+++ tools/training/clustering/clustering_graph_trainer.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "openzl/common/allocation.h"
+#include "openzl/compress/cgraph.h"
 #include "openzl/compress/graphs/generic_clustering_graph.h"
 #include "openzl/cpp/CCtx.hpp"
 #include "openzl/cpp/Exception.hpp"
@@ -31,18 +32,14 @@ namespace {
  * NOTE: this function does not swap this new clustering graph into the
  * original graph, it just registers it on the compressor.
  */
-std::string addAceSuccessors(
-        Compressor& compressor,
-        ZL_GraphID trainedClusteringGraphID)
+void setAceSuccessors(Compressor& compressor, GraphID trainedClusteringGraphID)
 {
     // Add the same number of ACE successors as there are clustering
     // successors
-    auto numClusteringSuccessors =
-            extractSuccessorsFromCbor(
-                    compressor.serialize(),
-                    ZL_Compressor_Graph_getName(
-                            compressor.get(), trainedClusteringGraphID))
-                    .size();
+    const size_t numClusteringSuccessors =
+            ZL_Compressor_Graph_getCustomGraphs(
+                    compressor.get(), trainedClusteringGraphID)
+                    .nbGraphIDs;
 
     std::vector<ZL_GraphID> aceGraphIds;
     aceGraphIds.reserve(numClusteringSuccessors);
@@ -50,24 +47,13 @@ std::string addAceSuccessors(
         aceGraphIds.push_back(ZL_Compressor_buildACEGraph(compressor.get()));
     }
 
-    // Create and register new clustering graph with the ACE successors
-    auto localParams = ZL_Compressor_Graph_getLocalParams(
-            compressor.get(), trainedClusteringGraphID);
-
-    ZL_ParameterizedGraphDesc const newClusteringGraphDesc = {
-        .graph          = trainedClusteringGraphID,
+    ZL_GraphParameters params = {
         .customGraphs   = aceGraphIds.data(),
         .nbCustomGraphs = aceGraphIds.size(),
-        .localParams    = &localParams
     };
 
-    // Replace original clustering graph with the new one
-    auto clusterGraphWithAceSuccessorsID =
-            ZL_Compressor_registerParameterizedGraph(
-                    compressor.get(), &newClusteringGraphDesc);
-
-    return ZL_Compressor_Graph_getName(
-            compressor.get(), clusterGraphWithAceSuccessorsID);
+    compressor.unwrap(ZL_Compressor_overrideGraphParams(
+            compressor.get(), trainedClusteringGraphID, &params));
 }
 
 /**
@@ -80,33 +66,22 @@ ZL_GraphID clusterSuccessors(
         const std::vector<MultiInput>& inputs,
         Compressor& compressor,
         const TrainParams& trainParams,
-        const std::string& clusteringGraphUniqueNameUntrained)
+        GraphID clusteringGraphUniqueIDUntrained)
 {
-    auto cctx                                = refCCtxForTraining(compressor);
-    const auto serializedCompressorUntrained = compressor.serialize();
+    auto cctx = refCCtxForTraining(compressor);
+
+    const std::string clusteringGraphUniqueNameUntrained =
+            ZL_Compressor_Graph_getName(
+                    compressor.get(), clusteringGraphUniqueIDUntrained);
 
     // Get successors for training
-    auto successorsFromSerialization = extractSuccessorsFromCbor(
-            serializedCompressorUntrained, clusteringGraphUniqueNameUntrained);
-    std::vector<ZL_GraphID> successorsVec;
-    successorsVec.reserve(successorsFromSerialization.size());
-    for (const auto& successor : successorsFromSerialization) {
-        ZL_GraphID graphId =
-                ZL_Compressor_getGraph(compressor.get(), successor.c_str());
-        successorsVec.push_back(graphId);
-    }
+    const auto successorsVec =
+            getCustomGraphs(compressor, clusteringGraphUniqueIDUntrained);
 
     // Get clustering codecs for training
-    auto nodesFromSerialization = extractNodesFromCbor(
-            serializedCompressorUntrained, CLUSTERING_GRAPH_NAME);
+    std::vector<ZL_NodeID> clusteringCodecs =
+            getCustomNodes(compressor, clusteringGraphUniqueIDUntrained);
 
-    std::vector<ZL_NodeID> clusteringCodecs;
-    clusteringCodecs.reserve(nodesFromSerialization.size());
-    for (const auto& node : nodesFromSerialization) {
-        ZL_NodeID nodeID =
-                ZL_Compressor_getNode(compressor.get(), node.c_str());
-        clusteringCodecs.push_back(nodeID);
-    }
     auto samples = collectInputStreamsForGraph(
             inputs, clusteringGraphUniqueNameUntrained, cctx);
     Logger::log_c(
@@ -137,17 +112,50 @@ ZL_GraphID clusterSuccessors(
  * Get the unique name of the clustering graph in the compressor's
  * graph. There is expected to be exactly one clustering graph.
  */
-std::string getClusteringGraphUniqueName(Compressor& compressor)
+ZL_GraphID getClusteringGraphUniqueID(const Compressor& compressor)
 {
-    auto clusteringGraphNames = findAllGraphsWithPrefix(
-            compressor.serialize(), CLUSTERING_GRAPH_NAME);
-    if (clusteringGraphNames.size() != 1) {
+    auto clusteringGraphs =
+            findAllGraphsWithPrefix(compressor, CLUSTERING_GRAPH_NAME);
+    if (clusteringGraphs.size() != 1) {
         throw Exception(
                 "Graph must contain a single clustering graph, instead it contains "
-                + std::to_string(clusteringGraphNames.size())
+                + std::to_string(clusteringGraphs.size())
                 + " clustering graphs");
     }
-    return clusteringGraphNames[0];
+    auto baseGraph = ZL_Compressor_Graph_getBaseGraphID(
+            compressor.get(), clusteringGraphs[0]);
+    if (baseGraph != ZL_GRAPH_CLUSTERING) {
+        throw Exception(
+                "Clustering graph is not a parameterized version of ZL_GRAPH_CLUSTERING");
+    }
+
+    return clusteringGraphs[0];
+}
+
+void overrideClusteringGraphParams(
+        Compressor& compressor,
+        GraphID untrainedGraph,
+        GraphID trainedGraph)
+{
+    assert(ZL_Compressor_Graph_getBaseGraphID(compressor.get(), untrainedGraph)
+           == ZL_GRAPH_CLUSTERING);
+    assert(ZL_Compressor_Graph_getBaseGraphID(compressor.get(), trainedGraph)
+           == ZL_GRAPH_CLUSTERING);
+
+    auto graphs = getCustomGraphs(compressor, trainedGraph);
+    auto nodes  = getCustomNodes(compressor, trainedGraph);
+    auto localParams =
+            ZL_Compressor_Graph_getLocalParams(compressor.get(), trainedGraph);
+
+    const ZL_GraphParameters params = {
+        .customGraphs   = graphs.data(),
+        .nbCustomGraphs = graphs.size(),
+        .customNodes    = nodes.data(),
+        .nbCustomNodes  = nodes.size(),
+        .localParams    = &localParams,
+    };
+    compressor.unwrap(ZL_Compressor_overrideGraphParams(
+            compressor.get(), untrainedGraph, &params));
 }
 
 } // namespace
@@ -159,33 +167,32 @@ std::shared_ptr<const std::string_view> trainClusteringGraph(
 {
     // Get name of untrained clustering graph which will be replaced in the
     // final trained graph
-    const auto clusteringGraphUniqueNameUntrained =
-            getClusteringGraphUniqueName(compressor);
+    const auto clusteringGraphUniqueIDUntrained =
+            getClusteringGraphUniqueID(compressor);
 
     // Cluster successors (or don't cluster)
     ZL_GraphID trainedClusteringGraphID = trainParams.noClustering
-            ? ZL_Compressor_getGraph(
-                      compressor.get(),
-                      clusteringGraphUniqueNameUntrained.c_str())
+            ? clusteringGraphUniqueIDUntrained
             : clusterSuccessors(
                       inputs,
                       compressor,
                       trainParams,
-                      clusteringGraphUniqueNameUntrained);
+                      clusteringGraphUniqueIDUntrained);
 
-    // Replace default successors with ACE successors or keep original name
-    const std::string clusteringGraphUniqueNameFinal =
-            trainParams.noAceSuccessors
-            ? ZL_Compressor_Graph_getName(
-                      compressor.get(), trainedClusteringGraphID)
-            : addAceSuccessors(compressor, trainedClusteringGraphID);
+    if (!trainParams.noAceSuccessors) {
+        // Replace default successors with ACE successors by overriding
+        // graph parameters
+        setAceSuccessors(compressor, trainedClusteringGraphID);
+    }
 
     // Replace original clustering graph with the new one that uses
-    // clustering and/or ACE successors
-    return createSharedStringView(renameGraphInCompressor(
-            compressor.serialize(),
-            clusteringGraphUniqueNameUntrained,
-            clusteringGraphUniqueNameFinal));
+    // clustering and/or ACE successors by overriding graph parameters
+    overrideClusteringGraphParams(
+            compressor,
+            clusteringGraphUniqueIDUntrained,
+            trainedClusteringGraphID);
+
+    return createSharedStringView(compressor.serialize());
 }
 
 } // namespace openzl::training
